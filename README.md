c1b01fab2997554efb80a3ca116600235fce6793
Adrian Kumpf <adrian@peaksandpies.com>
2018-07-12 17:27:43 +0200

    Add documentation, README and LICENSE

--- README.md
+++ README.md
@@ -4,76 +4,38 @@ The JavaScript SDK for [Bunchbox](https://bunchbox.co/)
 
 ## Getting Started
 
-These instructions will get you a copy of the project up and running on your
-local machine for development and testing purposes. See deployment for notes on
-how to deploy the project on a live system.
-
 ### Prerequisites
 
-What things you need to install the software and how to install them
-
-```
-
-```
+The SDK currently supports all versions of Node from Node v7 onwards.
 
 ### Installing
 
-A step by step series of examples that tell you how to get a development env running
+Add `@bunchbox/bunchbox-sdk` to your package.json:
 
-Say what the step will be
-
-```bash
-npm i
-```
-
-End with an example of getting some data out of the system or using it for a little demo
+`bash npm i @bunchbox/bunchbox-sdk`
 
 ## Running the tests
 
-Explain how to run the automated tests for this system
+Run the unit test suite with:
 
-```bash
-npm test
-```
+`bash npm test`
 
 ## Running the linter
 
-Explain what these tests test and why
-
-```bash
-npm run lint
-```
+`bash npm run lint`
 
 ## Building the docs
 
-Explain what these do and why
+To serve the docs at [https://localhost:4001](https://localhost:4001) run:
 
-```bash
-npm run docs
-```
-
-Then open https://localhost:4001
-
-## Built With
-
-- [Dropwizard](http://www.dropwizard.io/1.0.2/docs/) - The web framework used
-- [Maven](https://maven.apache.org/) - Dependency Management
-- [ROME](https://rometools.github.io/rome/) - Used to generate RSS Feeds
-
-## Contributing
-
-Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+`bash npm run docs`
 
 ## Versioning
 
-We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags).
+We use [SemVer](http://semver.org/) for versioning. For the versions available,
+see the [tags on this repository](https://github.com/your/project/tags).
 
 ## License
 
-This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
-
-## Acknowledgments
-
-- Hat tip to anyone whose code was used
-- Inspiration
-- etc
+This project is licensed under the MIT License - see the
+[LICENSE.md](LICENSE.md) file for details
--- test/tapes/e32ec051999f93a71783412158f39f32.js
+++ test/tapes/e32ec051999f93a71783412158f39f32.js
@@ -1,4 +1,4 @@
-var path = require("path");
+var path = require('path')
 
 /**
  * GET /v1/testing-file
@@ -10,28 +10,38 @@ var path = require("path");
  * connection: close
  */
 
-module.exports = function (req, res) {
-  res.statusCode = 403;
+module.exports = function(req, res) {
+  res.statusCode = 403
 
-  res.setHeader("server", "nginx");
-  res.setHeader("date", "Tue, 10 Jul 2018 13:45:59 GMT");
-  res.setHeader("content-type", "application/json; charset=utf-8");
-  res.setHeader("content-length", "68");
-  res.setHeader("connection", "close");
-  res.setHeader("vary", "Accept-Encoding");
-  res.setHeader("x-dns-prefetch-control", "off");
-  res.setHeader("x-frame-options", "SAMEORIGIN");
-  res.setHeader("strict-transport-security", "max-age=15552000; includeSubDomains");
-  res.setHeader("x-download-options", "noopen");
-  res.setHeader("x-content-type-options", "nosniff");
-  res.setHeader("x-xss-protection", "1; mode=block");
-  res.setHeader("set-cookie", ["lang=en; path=/; expires=Wed, 10 Jul 2019 13:45:59 GMT; domain=bunchbox.co"]);
-  res.setHeader("etag", "W/\"44-CiSBwvIRw3ymqfvXCsXwTQ8leBE\"");
+  res.setHeader('server', 'nginx')
+  res.setHeader('date', 'Tue, 10 Jul 2018 13:45:59 GMT')
+  res.setHeader('content-type', 'application/json; charset=utf-8')
+  res.setHeader('content-length', '68')
+  res.setHeader('connection', 'close')
+  res.setHeader('vary', 'Accept-Encoding')
+  res.setHeader('x-dns-prefetch-control', 'off')
+  res.setHeader('x-frame-options', 'SAMEORIGIN')
+  res.setHeader(
+    'strict-transport-security',
+    'max-age=15552000; includeSubDomains'
+  )
+  res.setHeader('x-download-options', 'noopen')
+  res.setHeader('x-content-type-options', 'nosniff')
+  res.setHeader('x-xss-protection', '1; mode=block')
+  res.setHeader('set-cookie', [
+    'lang=en; path=/; expires=Wed, 10 Jul 2019 13:45:59 GMT; domain=bunchbox.co'
+  ])
+  res.setHeader('etag', 'W/"44-CiSBwvIRw3ymqfvXCsXwTQ8leBE"')
 
-  res.setHeader("x-yakbak-tape", path.basename(__filename, ".js"));
+  res.setHeader('x-yakbak-tape', path.basename(__filename, '.js'))
 
-  res.write(new Buffer("eyJzdWNjZXNzIjpmYWxzZSwiZXJyb3IiOiJJbnZhbGlkIHRva2VuIiwiZXJyb3JzIjpbIkludmFsaWQgdG9rZW4iXX0=", "base64"));
-  res.end();
+  res.write(
+    new Buffer(
+      'eyJzdWNjZXNzIjpmYWxzZSwiZXJyb3IiOiJJbnZhbGlkIHRva2VuIiwiZXJyb3JzIjpbIkludmFsaWQgdG9rZW4iXX0=',
+      'base64'
+    )
+  )
+  res.end()
 
-  return __filename;
-};
+  return __filename
+}
